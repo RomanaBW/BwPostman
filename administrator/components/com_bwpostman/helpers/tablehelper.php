d95a02f787798ba58f83a068d29ba6098cd9b1fd
Romana Boldt <webmaster@boldt-webservice.de>
2015-12-14 20:35:28 +0100

    Removed some unnecessary code, JURI::base(true)  removed from addStyleSheet

--- administrator/components/com_bwpostman/helpers/tablehelper.php
+++ administrator/components/com_bwpostman/helpers/tablehelper.php
@@ -1306,8 +1306,8 @@ abstract class BwPostmanTableHelper {
 	/**
 	 * Builds the XML data to export
 	 *
-	 * @param	string	$tableName  name of table
-	 * @param	handle	$fp         handle of backup file
+	 * @param	string	        $tableName  name of table
+	 * @param	file pointer	$fp         handle of backup file
 	 *
 	 * @return	array	An array of XML lines (strings).
 	 *

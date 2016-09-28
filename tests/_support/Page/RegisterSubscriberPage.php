71ad2b856398d0fb86e9db03430a5e8444d32627
Romana Boldt <webmaster@boldt-webservice.de>
2016-09-28 16:16:10 +0200

    <version 2.0.0 --28-Sept-2016> variables sorted additional variables for further tests

--- tests/_support/Page/RegisterSubscriberPage.php
+++ tests/_support/Page/RegisterSubscriberPage.php
@@ -29,9 +29,9 @@ class RegisterSubscriberPage
 	 * Declare UI map for this page here. CSS or XPath allowed.
 	 */
 
+	// Frontend stuff
 	// used urls and links
     public static $register_url         = '/index.php?option=com_users&view=registration';
-	public static $user_management_url  = 'administrator/index.php?option=com_users&view=users';
 	public static $user_activation_url  = '/index.php?option=com_users&task=registration.activate&token=';
 
 	//view identifier
@@ -51,10 +51,6 @@ class RegisterSubscriberPage
 	public static $login_identifier_register        = ".//*[@id='member-registration']/div/div/button";
 	public static $login_identifier_cancel          = ".//*[@id='member-registration']/div/div/a";
 
-	// success message identifiers
-	public static $success_heading_identifier   = ".//*[@id='system-message']/div/h4";
-	public static $success_message_identifier   = "div.alert-success > div > div.alert-message";
-
 	// login field values
 	public static $login_value_name         = "Sam Sample";
 	public static $login_value_username     = "Sam";
@@ -63,36 +59,62 @@ class RegisterSubscriberPage
 
 	public static $change_value_email       = "dummy-2@bwtest.nil";
 
+	// success message identifiers
+	public static $success_heading_identifier   = ".//*[@id='system-message']/div/h4";
+	public static $success_message_identifier   = "div.alert-success > div > div.alert-message";
 	public static $activation_completed_text    = "Your Account has been successfully activated. You can now log in using the username and password you chose during the registration.";
 	public static $activation_complete          = ".//*[@id='system-message']/div/div/div";
 
 
-	// backend
-	public static $user_filter_col_identifier   = ".//*[@id='userList']/tbody/*/td[2]";
-	public static $delete_button                = ".//*[@id='toolbar-delete']/button";
-
+	// backend stuff
+	public static $user_management_url              = 'administrator/index.php?option=com_users&view=users';
 	public static $subscribers_url                  = "/administrator/index.php?option=com_bwpostman&view=subscribers";
+	public static $plugin_page                      = "/administrator/index.php?option=com_plugins";
+
+	// com_users related
+	public static $user_filter_col_identifier       = ".//*[@id='userList']/tbody/*/td[2]";
+	public static $user_edit_identifier             = ".//*[@id='userList']/tbody/*/td[2]/div[1]/a";
+	public static $toolbar_save_button              = ".//*[@id='toolbar-save']/button";
+	public static $toolbar_delete_button            = ".//*[@id='toolbar-delete']/button";
+
+	// com_bwpostman related
 	public static $tab_confirmed                    = ".//*[@id='bwpostman_subscribers_tabs']/dt[2]";
 	public static $tab_unconfirmed                  = ".//*[@id='bwpostman_subscribers_tabs']/dt[3]";
+	public static $subscriber_email_col_identifier  = ".//*[@id='j-main-container']/div[2]/div/dd[%s]/table/tbody/*/td[5]";
 	public static $subscriber_format_col_identifier = ".//*[@id='j-main-container']/div[2]/div/dd[%s]/table/tbody/*/td[6]";
 	public static $subscriber_userid_col_identifier = ".//*[@id='j-main-container']/div[2]/div/dd[%s]/table/tbody/*/td[7]";
 	public static $subscriber_filter_col_identifier = ".//*[@id='j-main-container']/div[2]/div/dd[%s]/table/tbody/*/td[2]";
-	public static $subscriber_edit_link             = ".//*[@id='j-main-container']/div[2]/div/dd[%s]/table/tbody/tr/td[2]/a";
+	public static $subscriber_edit_link             = ".//*[@id='j-main-container']/div[2]/div/dd[%s]/table/tbody/*/td[2]/a";
 
+	// mailinglist check
 	public static $available_mailinglists_label     = ".//*[@id='adminForm']/div[1]/div[1]/fieldset/legend";
-//	public static $mailinglist1_checked             = ".//*[@id='jform_ml_intern_2']";
 	public static $mailinglist1_checked             = ".//*[@id='jform_ml_available_1']";
-//	public static $mailinglist1_checked             = ".//input[@name='jform[ml_unavailable][]]']/following::*/span[contains(text(),'01 Mailingliste 3 A')]";
 	public static $mailinglist2_checked             = ".//*[@id='jform_ml_available_4']";
+	public static $mailinglist_fieldset_identifier  = "//*[@id='adminForm']/div[1]/div[1]/fieldset/div[1]/div/fieldset/legend/span[2]";
 	public static $subscriber_details_close         = ".//*[@id='toolbar-cancel']/button";
+
+	// search subscriber
+	public static $search_tool_button               = ".//*[@id='j-main-container']/div[1]/div[1]/div[1]/div[2]/button";
+	public static $search_for_list                  = ".//*[@id='filter_search_filter_chzn']";
+	public static $search_for_value                 = ".//*[@id='filter_search_filter_chzn']/div/ul/li[contains(text(), 'Name')]";
+
+	// check for selected options
+	public static $user_id_identifier               = ".//*[@id='j-main-container']/div[2]/div/dd[1]/table/tbody/tr/td[7]";
+	public static $email_identifier                 = ".//*[@id='userList']/tbody/tr[1]/td[7]";
+	public static $mail_field_identifier            = ".//*[@id='jform_email']";
+
+	// com_plugin related
+	public static $view_plugin                      = "Plugins";
+	public static $plugin_name                      = "BwPostman Plugin RegisterSubscribe";
+	public static $search_plugin_group_list         = ".//*[@id='filter_folder_chzn']/a";
+	public static $search_group_value               = ".//*[@id='filter_folder_chzn']/div/ul/li[text()='system']";
+	public static $icon_published_identifier        = ".//*[@id='pluginList']/tbody/tr/td[3]/a/span";
 
 	//messages
 	public static $delete_confirm           = "Are you sure you want to delete? Confirming will permanently delete the selected item(s)!";
 	public static $delete_success           = "1 user successfully deleted.";
 	public static $register_success         = "Your account has been created and an activation link has been sent";
 
-	// error message identifier
-
-
-	// invalid fields
+	public static $plugin_disabled_success  = 'Plugin successfully disabled';
+	public static $plugin_enabled_success   = 'Plugin successfully enabled';
 }

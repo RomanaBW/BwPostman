406d2cfd8c1b7d19bfd94b5271335fc38ef047ff
Romana Boldt <webmaster@boldt-webservice.de>
2016-09-30 19:29:48 +0200

    <version 2.0.0 --30-Sept-2016> Method getLabel() removed

--- components/com_bwpostman/models/fields/comml_noarc.php
+++ components/com_bwpostman/models/fields/comml_noarc.php
@@ -57,59 +57,6 @@ class JFormFieldComMl extends JFormFieldCheckboxes
 	protected $forceMultiple = true;
 
 	/**
-	 * Method to get the field label markup.
-	 *
-	 * @return  string  The field label markup.
-	 *
-	 * @since   11.1
-	 */
-	protected function getLabel()
-	{
-		// Initialise variables.
-		$label = '';
-
-		if ($this->hidden)
-		{
-			return $label;
-		}
-
-		// Get the label text from the XML element, defaulting to the element name.
-		$text = $this->element['label'] ? (string) $this->element['label'] : (string) $this->element['name'];
-		$text = $this->translateLabel ? JText::_($text) : $text;
-
-		// Build the class for the label.
-		$class = !empty($this->description) ? 'hasTip hasTooltip' : '';
-		$class = $this->required == true ? $class . ' required' : $class;
-		$class = !empty($this->labelClass) ? $class . ' ' . $this->labelClass : $class;
-
-		// Add the opening label tag and main attributes attributes.
-		$label .= '<label id="' . $this->id . '-lbl" for="' . $this->id . '" class="' . $class . '"';
-
-		// If a description is specified, use it to build a tooltip.
-		if (!empty($this->description))
-		{
-			$label .= ' title="'
-				. htmlspecialchars(
-				trim($text, ':') . '<br />' . ($this->translateDescription ? JText::_($this->description) : $this->description),
-				ENT_COMPAT, 'UTF-8'
-			) . '"';
-		}
-
-		// Add the label text and closing tag.
-		if ($this->required)
-		{
-			$label .= '>' . $text . '<span class="star">&#160;*</span></label>';
-		}
-		else
-		{
-			$label .= '>' . $text . '</label><br />';
-		}
-
-		return $label;
-	}
-
-
-	/**
 	 * Method to get the field input markup for check boxes.
 	 *
 	 * @return  string  The field input markup.

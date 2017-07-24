2f0c39e3b2f2fd9c7df623efa1cdfcd037ea4ab8
Romana Boldt <webmaster@boldt-webservice.de>
2017-07-24 22:15:56 +0200

    <2017-07-24> initial commit test mode plugin

---
+++ plugins/system/bwtests/bwtests.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * BW Tests Plugin
+ *
+ * BW Tests Plugin main file for BwPostman.
+ *
+ * @version 1.0.0 bwpte
+ * @package			BW Tests Plugin
+ * @author			Romana Boldt
+ * @copyright		(C) 2017 Boldt Webservice <forum@boldt-webservice.de>
+ * @support https://www.boldt-webservice.de/en/forum-en/bwpostman.html
+ * @license			GNU/GPL v3, see LICENSE.txt
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+jimport('joomla.plugin.plugin');
+
+
+/**
+ * Class plgBwTests
+ *
+ * @since       2.0.0
+ */
+class plgBwTests extends JPlugin
+{
+	// only a dummy
+}
+

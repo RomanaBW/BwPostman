7bd15abbdc2fea1a9b8f17bc9033a68e29a7d2e7
Romana Boldt <webmaster@boldt-webservice.de>
2017-07-24 22:16:22 +0200

    <2017-07-24> initial commit test mode plugin

---
+++ plugins/system/bwtests/installscript.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * BW Tests Plugin
+ *
+ * BW Tests Plugin installer.
+ *
+ * @version 1.0.0 bwpte
+ * @package			BW Tests Plugin
+ * @author			Romana Boldt
+ * @copyright		(C) 2016-2017 Boldt Webservice <forum@boldt-webservice.de>
+ * @support https://www.boldt-webservice.de/en/forum-en/bwpostman.html
+ * @license			GNU/GPL v3, see LICENSE.txt
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+/**
+ * Script file of BwPostman module
+ *
+ * @since       2.0.0
+ */
+class plgBwTestsInstallerScript
+{
+  /**
+   * Method to install the extension
+   *
+   * @param object  $parent is the class calling this method
+   *
+   * @return void
+   *
+   * @since       2.0.0
+   */
+  function install($parent)
+  {
+//    $this->showFinished(false);
+  }
+
+  /**
+   * Method to uninstall the extension
+   *
+   * @return void
+   *
+   * @since       2.0.0
+  */
+  function uninstall()
+  {
+		JFactory::getApplication()->enqueueMessage(JText::_('PLG_BW_PLUGIN_TESTS_UNINSTALL_THANKYOU'), 'message');
+  }
+
+  /**
+   * Method to update the extension
+   *
+   * @param object  $parent is the class calling this method
+
+   * @return void
+   *
+   * @since       2.0.0
+  */
+  function update($parent)
+  {
+//		$this->showFinished(true);
+  }
+
+  /**
+   * Method to run after an install/update/uninstall method
+   *
+   * @param string  $type       is the type of change (install, update or discover_install)
+
+   * @return void
+   *
+   * @since       2.0.0
+  */
+  function postflight($type)
+  {
+	  // We only need to perform this if the extension is being installed, not updated
+	  if ( $type == 'install' )
+	  {
+		  $db = JFactory::getDbo();
+		  $query = $db->getQuery(true);
+
+		  $fields = array(
+			  $db->quoteName('enabled') . ' = ' . (int) 1,
+			  $db->quoteName('ordering') . ' = ' . (int) 9999
+        );
+
+        $conditions = array(
+	        $db->quoteName('element') . ' = ' . $db->quote('bwtests'),
+	        $db->quoteName('folder') . ' = ' . $db->quote('system'),
+	        $db->quoteName('type') . ' = ' . $db->quote('plugin')
+        );
+
+        $query->update($db->quoteName('#__extensions'))->set($fields)->where($conditions);
+
+        $db->setQuery($query);
+        $db->execute();
+    }
+  }
+}

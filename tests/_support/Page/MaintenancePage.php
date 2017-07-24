2358969c50e16bb7aef6cdca7f8fcf2fe8dc6791
Romana Boldt <webmaster@boldt-webservice.de>
2017-07-24 21:16:30 +0200

    <2017-07-24> identifier and switch for tests with version 1.3.2 added

--- tests/_support/Page/MaintenancePage.php
+++ tests/_support/Page/MaintenancePage.php
@@ -93,7 +93,16 @@ class MaintenancePage
 		$I->click(MaintenancePage::$restoreTablesButton);
 		$I->waitForElement(MaintenancePage::$headingRestoreFile, 30);
 
-		$I->attachFile(".//*[@id='restorefile']", "BwPostman_2_0_0_Tables.xml");
+		$bwpm_version   = getenv('BW_TEST_BWPM_VERSION');
+		if ($bwpm_version == '132')
+		{
+			$I->attachFile(".//*[@id='restorefile']", "BwPostman_1_3_2_Tables.xml");
+		}
+		else
+		{
+			$I->attachFile(".//*[@id='restorefile']", "BwPostman_2_0_0_Tables.xml");
+		}
+
 		$I->click(".//*[@id='adminForm']/fieldset/div[2]/div/table/tbody/tr[2]/td/input");
 		$I->dontSeeElement(Generals::$alert_error);
 

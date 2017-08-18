477553e2a89e185df7c75a0f902354533aab341f
Romana Boldt <webmaster@boldt-webservice.de>
2017-08-18 07:29:07 +0200

    <2017-08-17> own JS removed JS comparisons ensured modifications to meet Joomla standards

--- src/components/com_bwpostman/views/register/tmpl/default.php
+++ src/components/com_bwpostman/views/register/tmpl/default.php
@@ -39,129 +39,6 @@ require_once (JPATH_SITE . '/components/com_content/helpers/route.php');
 $remote_ip  = JFactory::getApplication()->input->server->get('REMOTE_ADDR', '', '');
 ?>
 
-<script type="text/javascript">
-/* <![CDATA[ */
-
-function checkRegisterForm() {
-
-	var form = document.bwp_com_form;
-	var errStr = "";
-	var arrCB = document.bwp_com_form.elements['mailinglists[]'];
-	var n =	arrCB.length;
-	var check = 0;
-
-	// Validate input fields
-  // firstname
-	if (document.bwp_com_form.firstname)
-	{
-		if (((document.bwp_com_form.getElementById("firstname").value == "" || (document.bwp_com_form.getElementById("firstname").value == "<?php echo JText::_('COM_BWPOSTMAN_FIRSTNAME'); ?>")))
-			&& (document.bwp_com_form.getElementById("firstname_field_obligation").value == 1))
-		{
-			errStr += "<?php echo JText::_('COM_BWPOSTMAN_ERROR_FIRSTNAME', true); ?>\n";
-		}
-	}
-  // name
-	if (document.bwp_com_form.name)
-	{
-		if (((document.bwp_com_form.getElementById("name").value == "") || (document.bwp_com_form.getElementById("name").value == "<?php echo JText::_('COM_BWPOSTMAN_NAME'); ?>"))
-			&& (document.bwp_com_form.getElementById("name_field_obligation").value == 1))
-		{
-			errStr += "<?php echo JText::_('COM_BWPOSTMAN_ERROR_NAME', true); ?>\n";
-		}
-	}
-	// additional field
-	if (document.bwp_com_form.special)
-	{
-		if (((document.bwp_com_form.getElementById("special").value == "") || (document.bwp_com_form.getElementById("special").value == "<?php echo JText::_($this->params->get('special_label')); ?>"))
-			&& (document.bwp_com_form.getElementById("special_field_obligation").value == 1))
-		{
-			errStr += "<?php echo JText::sprintf('COM_BWPOSTMAN_SUB_ERROR_SPECIAL', JText::_($this->params->get('special_label'))); ?>\n";
-		}
-	}
-  // email
-  var email = document.bwp_com_form.getElementById("email").value;
-	if (email == "" || (email == "<?php echo JText::_('COM_BWPOSTMAN_EMAIL'); ?>"))
-	{
-		errStr += "<?php echo JText::_('COM_BWPOSTMAN_ERROR_EMAIL', true); ?>\n";
-	}
-	else
-	{
-		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		if (!filter.test(email))
-		{
-			errStr += "<?php echo JText::_('COM_BWPOSTMAN_ERROR_EMAIL_INVALID', true); ?>\n";
-			email.focus;
-		}
-	}
-  // mailinglist
-	var i = 0;
-  	if (n > 1)
-    {
-		for (i = 0; i < n; i++)
-		{
-			if (arrCB[i].checked == true)
-			{
-				check++;
-			}
-		}
-	}
-	else
-    {
-		check++;
-	}
-
-	if (check == 0)
-	{
-		errStr += "<?php echo JText::_('COM_BWPOSTMAN_ERROR_NL_CHECK'); ?>\n";
-	}
-	// disclaimer
-	if (document.bwp_com_form.agreecheck)
-	{
-		if (document.bwp_com_form.agreecheck.checked == false)
-		{
-			errStr += "<?php echo JText::_('COM_BWPOSTMAN_ERROR_DISCLAIMER_CHECK'); ?>\n";
-		}
-	  }
-	  // captcha
-	if (document.bwp_com_form.stringCaptcha)
-	{
-		if (document.bwp_com_form.stringCaptcha.value == '')
-		{
-			errStr += "<?php echo JText::_('COM_BWPOSTMAN_ERROR_CAPTCHA_CHECK'); ?>\n";
-		}
-	}
-	// question
-	if (document.bwp_com_form.stringQuestion)
-	{
-		if (document.bwp_com_form.stringQuestion.value == '')
-		{
-			errStr += "<?php echo JText::_('COM_BWPOSTMAN_ERROR_CAPTCHA_CHECK'); ?>\n";
-		}
-	}
-	if ( errStr !== "" )
-	{
-		alert( errStr );
-		return false;
-	}
-	else
-	{
-		form.submit();
-	}
-}
-	/* ]]> */
-</script>
-
-<noscript>
-	<div id="system-message">
-		<div class="alert alert-warning">
-			<h4 class="alert-heading"><?php echo JText::_('WARNING'); ?></h4>
-			<div>
-				<p><?php echo JText::_('COM_BWPOSTMAN_JAVASCRIPTWARNING'); ?></p>
-			</div>
-		</div>
-	</div>
-</noscript>
-
 <div id="bwpostman">
 	<div id="bwp_com_register">
 		<?php // displays a message if no availible mailinglist
@@ -172,7 +49,7 @@ function checkRegisterForm() {
 			<?php endif; ?>
 
 		<div class="content_inner">
-			<form action="<?php echo JRoute::_('index.php?option=com_bwpostman&view=register'); ?>" method="post" id="bwp_com_form" name="bwp_com_form" class="form-validate form-inline" onsubmit="return checkRegisterForm();">
+			<form action="<?php echo JRoute::_('index.php?option=com_bwpostman&view=register.save'); ?>" method="post" id="bwp_com_form" name="bwp_com_form" class="form-validate form-inline">
 				<?php // Spamcheck 1 - Input-field: class="user_highlight" style="position: absolute; top: -5000px;" ?>
 				<p class="user_hightlight">
 					<label for="falle"><strong><?php echo addslashes(JText::_('COM_BWPOSTMAN_SPAMCHECK')); ?></strong></label>
@@ -289,7 +166,7 @@ jQuery(document).ready(function()
 			{
 				label.addClass('active btn-primary');
 			}
-			else if (input.val() == 0)
+			else if (input.val() === 0)
 			{
 				label.addClass('active btn-danger');
 			}
@@ -302,11 +179,11 @@ jQuery(document).ready(function()
 	});
 	jQuery(".btn-group input[checked=checked]").each(function()
 	{
-		if (jQuery(this).val() == '')
+		if (jQuery(this).val() === '')
 		{
 			jQuery("label[for=" + jQuery(this).attr('id') + "]").addClass('active btn-primary');
 		}
-		else if (jQuery(this).val() == 0)
+		else if (jQuery(this).val() === 0)
 		{
 			jQuery("label[for=" + jQuery(this).attr('id') + "]").addClass('active btn-danger');
 		}

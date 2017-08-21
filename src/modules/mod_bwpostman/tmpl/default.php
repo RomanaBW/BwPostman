6a2e50a06a33627b95a63a4efc8edde02a902e33
Romana Boldt <webmaster@boldt-webservice.de>
2017-08-21 10:10:55 +0200

    <2017-08-21> email check enhanced to meet new TLDs

--- src/modules/mod_bwpostman/tmpl/default.php
+++ src/modules/mod_bwpostman/tmpl/default.php
@@ -95,7 +95,7 @@ function checkModRegisterForm()
 	}
 	else
 	{
-	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,14})+$/;
 		if (!filter.test(email))
 		{
 			errStr += "<?php echo JText::_('MOD_BWPOSTMANERROR_EMAIL_INVALID', true); ?>\n";

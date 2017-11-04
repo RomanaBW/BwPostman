ce8e47bf106d8f0517f2050c74e3f33e75c840d7
Romana Boldt <webmaster@boldt-webservice.de>
2017-11-04 11:36:17 +0100

    Changed spelling from Joomla to Joomla!

--- tests/_support/Page/SubscriberManagerPage.php
+++ tests/_support/Page/SubscriberManagerPage.php
@@ -35,24 +35,24 @@ class SubscriberManagerPage
 	 *
 	 * @since   2.0.0
 	 */
-    public static $url      = '/administrator/index.php?option=com_bwpostman&view=subscribers';
+	public static $url      = '/administrator/index.php?option=com_bwpostman&view=subscribers';
 	public static $section  = 'subscriber';
 	public static $wait_db  = 1;
 
-    /*
-     * Declare UI map for this page here. CSS or XPath allowed.
-     * public static $usernameField = '#username';
-     * public static $formSubmitButton = "#mainForm input[type=submit]";
-     */
+	/*
+	 * Declare UI map for this page here. CSS or XPath allowed.
+	 * public static $usernameField = '#username';
+	 * public static $formSubmitButton = "#mainForm input[type=submit]";
+	 */
 
 	public static $tab_confirmed   = ".//*[@id='bwpostman_subscribers_tabs']/dt[2]";
 	public static $tab_unconfirmed = ".//*[@id='bwpostman_subscribers_tabs']/dt[3]";
-    public static $tab_testers     = ".//*[@id='bwpostman_subscribers_tabs']/dt[3]";
+	public static $tab_testers     = ".//*[@id='bwpostman_subscribers_tabs']/dt[3]";
 
-    public static $first_list_link          = ".//*[@id='main-table']/tbody/tr[1]/td[2]/a";
-    public static $first_list_entry_tab2    = ".//*[@id='ub0']";
+	public static $first_list_link          = ".//*[@id='main-table']/tbody/tr[1]/td[2]/a";
+	public static $first_list_entry_tab2    = ".//*[@id='ub0']";
 
-    // search subscriber
+	// search subscriber
 	public static $search_for_list_id               = "filter_search_filter_chzn";
 	public static $search_for_list                  = ".//*[@id='filter_search_filter_chzn']";
 	public static $search_for_value                 = ".//*[@id='filter_search_filter_chzn']/div/ul/li[contains(text(), '%s')]";
@@ -70,7 +70,7 @@ class SubscriberManagerPage
 			'gender'        => 'Gender',
 			'email'         => 'Email',
 			'Email format'  => 'Email format',
-			'user_id'       => 'Joomla User-ID',
+			'user_id'       => 'Joomla! User-ID',
 			'mailinglists'  => '# Mailinglists',
 			'id'            => 'ID',
 			'name'          => 'Last name'
@@ -81,7 +81,7 @@ class SubscriberManagerPage
 			'gender'        => 'Gender',
 			'email'         => 'Email',
 			'Email format'  => 'Mail format',
-			'user_id'       => 'Joomla User-ID',
+			'user_id'       => 'Joomla! User-ID',
 			'mailinglists'  => '# subscribed mailing lists',
 			'id'            => 'ID',
 			'name'          => 'Name'
@@ -192,135 +192,135 @@ class SubscriberManagerPage
 	);
 
 	public static $import_csv_button    = ".//*[@id='fileformatcsv']";
-    public static $import_xml_button    = ".//*[@id='fileformatxml']";
+	public static $import_xml_button    = ".//*[@id='fileformatxml']";
 
-    public static $import_csv_file    = "import_demo.csv";
-    public static $import_xml_file    = "import_demo.xml";
+	public static $import_csv_file    = "import_demo.csv";
+	public static $import_xml_file    = "import_demo.xml";
 
-    public static $import_search_button = ".//*[@id='importfile']";
+	public static $import_search_button = ".//*[@id='importfile']";
 
-    public static $import_csv_delimiter = ".//*[@id='delimiter_chzn']/a/span";
-    public static $import_csv_separator = ".//*[@id='enclosure_chzn']/a/span";
-    public static $import_csv_caption   = ".//*[@id='caption']";
+	public static $import_csv_delimiter = ".//*[@id='delimiter_chzn']/a/span";
+	public static $import_csv_separator = ".//*[@id='enclosure_chzn']/a/span";
+	public static $import_csv_caption   = ".//*[@id='caption']";
 
-    public static $import_button_further = ".//*[@id='adminForm']/fieldset/div/div/table/tbody/tr[6]/td/input";
-    public static $import_button_import  = ".//*[@id='adminForm']/fieldset[2]/div/table/tbody/tr/td/input";
+	public static $import_button_further = ".//*[@id='adminForm']/fieldset/div/div/table/tbody/tr[6]/td/input";
+	public static $import_button_import  = ".//*[@id='adminForm']/fieldset[2]/div/table/tbody/tr/td/input";
 
-    public static $import_legend_step_2 = ".//*[@id='adminForm']/fieldset[2]/legend";
-    public static $import_legend_mls    = ".//*[@id='adminForm']/fieldset[2]/div/div[2]/fieldset/div[1]/div/fieldset/legend/span[2]";
-    public static $import_legend_format = ".//*[@id='adminForm']/fieldset[2]/div/div[3]";
+	public static $import_legend_step_2 = ".//*[@id='adminForm']/fieldset[2]/legend";
+	public static $import_legend_mls    = ".//*[@id='adminForm']/fieldset[2]/div/div[2]/fieldset/div[1]/div/fieldset/legend/span[2]";
+	public static $import_legend_format = ".//*[@id='adminForm']/fieldset[2]/div/div[3]";
 
-    public static $import_cb_text_format  = ".//*[@id='emailformat0']";
-    public static $import_cb_html_format  = ".//*[@id='emailformat1']";
-    public static $import_cb_confirm_subs = ".//*[@id='confirm']";
+	public static $import_cb_text_format  = ".//*[@id='emailformat0']";
+	public static $import_cb_html_format  = ".//*[@id='emailformat1']";
+	public static $import_cb_confirm_subs = ".//*[@id='confirm']";
 
-    public static $import_csv_field_0   = "Column_0 (name)";
-    public static $import_csv_field_1   = "Column_1 (firstname)";
-    public static $import_csv_field_2   = "Column_2 (email)";
-    public static $import_csv_field_3   = "Column_3 (emailformat)";
-    public static $import_csv_field_4   = "Column_4 (status)";
+	public static $import_csv_field_0   = "Column_0 (name)";
+	public static $import_csv_field_1   = "Column_1 (firstname)";
+	public static $import_csv_field_2   = "Column_2 (email)";
+	public static $import_csv_field_3   = "Column_3 (emailformat)";
+	public static $import_csv_field_4   = "Column_4 (status)";
 
-    public static $import_xml_field_0   = "Field_0 (name)";
-    public static $import_xml_field_1   = "Field_1 (firstname)";
-    public static $import_xml_field_2   = "Field_2 (email)";
-    public static $import_xml_field_3   = "Field_3 (emailformat)";
-    public static $import_xml_field_4   = "Field_4 (status)";
+	public static $import_xml_field_0   = "Field_0 (name)";
+	public static $import_xml_field_1   = "Field_1 (firstname)";
+	public static $import_xml_field_2   = "Field_2 (email)";
+	public static $import_xml_field_3   = "Field_3 (emailformat)";
+	public static $import_xml_field_4   = "Field_4 (status)";
 
-    public static $import_csv_subscribers   = array(
-        array(
-            'name' => 'Muster',
-            'firstname' => 'Max',
-            'email' => 'test1@test-domain.nul',
-            'emailformat' => '1',
-            'status' => '1',
-        ),
-        array(
-            'name' => 'Muster',
-            'firstname' => 'Moritz',
-            'email' => 'test2@test-domain.nul',
-            'emailformat' => '1',
-            'status' => '1',
-        ),
-        array(
-            'name' => 'Muster',
-            'firstname' => 'Brunhilde',
-            'email' => 'test3@test-domain.nul',
-            'emailformat' => '1',
-            'status' => '1',
-        ),
-        array(
-            'name' => 'Muster',
-            'firstname' => 'Adelgunde',
-            'email' => 'test4@test-domain.nul',
-            'emailformat' => '1',
-            'status' => '1',
-        ),
-        array(
-            'name' => 'Muster',
-            'firstname' => 'Erika',
-            'email' => 'test5@test-domain.nul',
-            'emailformat' => '1',
-            'status' => '1',
-        ),
-        array(
-            'name' => 'Muster',
-            'firstname' => 'Eugen',
-            'email' => 'test6@test-domain.nul',
-            'emailformat' => '0',
-            'status' => '1',
-        ),
-    );
+	public static $import_csv_subscribers   = array(
+		array(
+			'name' => 'Muster',
+			'firstname' => 'Max',
+			'email' => 'test1@test-domain.nul',
+			'emailformat' => '1',
+			'status' => '1',
+		),
+		array(
+			'name' => 'Muster',
+			'firstname' => 'Moritz',
+			'email' => 'test2@test-domain.nul',
+			'emailformat' => '1',
+			'status' => '1',
+		),
+		array(
+			'name' => 'Muster',
+			'firstname' => 'Brunhilde',
+			'email' => 'test3@test-domain.nul',
+			'emailformat' => '1',
+			'status' => '1',
+		),
+		array(
+			'name' => 'Muster',
+			'firstname' => 'Adelgunde',
+			'email' => 'test4@test-domain.nul',
+			'emailformat' => '1',
+			'status' => '1',
+		),
+		array(
+			'name' => 'Muster',
+			'firstname' => 'Erika',
+			'email' => 'test5@test-domain.nul',
+			'emailformat' => '1',
+			'status' => '1',
+		),
+		array(
+			'name' => 'Muster',
+			'firstname' => 'Eugen',
+			'email' => 'test6@test-domain.nul',
+			'emailformat' => '0',
+			'status' => '1',
+		),
+	);
 
-    public static $import_xml_subscribers   = array(
-        array(
-            'name' => 'Muster',
-            'firstname' => 'Maximilian',
-            'email' => 'test7@test-domain.nul',
-            'emailformat' => '1',
-            'status' => '1',
-        ),
-        array(
-            'name' => 'Muster',
-            'firstname' => 'Emil',
-            'email' => 'test8@test-domain.nul',
-            'emailformat' => '1',
-            'status' => '1',
-        ),
-        array(
-            'name' => 'Muster',
-            'firstname' => 'Hanni',
-            'email' => 'test9@test-domain.nul',
-            'emailformat' => '1',
-            'status' => '1',
-        ),
-        array(
-            'name' => 'Muster',
-            'firstname' => 'Nanni',
-            'email' => 'test10@test-domain.nul',
-            'emailformat' => '1',
-            'status' => '1',
-        ),
-    );
+	public static $import_xml_subscribers   = array(
+		array(
+			'name' => 'Muster',
+			'firstname' => 'Maximilian',
+			'email' => 'test7@test-domain.nul',
+			'emailformat' => '1',
+			'status' => '1',
+		),
+		array(
+			'name' => 'Muster',
+			'firstname' => 'Emil',
+			'email' => 'test8@test-domain.nul',
+			'emailformat' => '1',
+			'status' => '1',
+		),
+		array(
+			'name' => 'Muster',
+			'firstname' => 'Hanni',
+			'email' => 'test9@test-domain.nul',
+			'emailformat' => '1',
+			'status' => '1',
+		),
+		array(
+			'name' => 'Muster',
+			'firstname' => 'Nanni',
+			'email' => 'test10@test-domain.nul',
+			'emailformat' => '1',
+			'status' => '1',
+		),
+	);
 
-    public static $import_mls_target    = ".//*[@id='adminForm']/fieldset[2]/div/div[2]/fieldset/div[1]/div/fieldset/div/p[2]/label";
+	public static $import_mls_target    = ".//*[@id='adminForm']/fieldset[2]/div/div[2]/fieldset/div[1]/div/fieldset/div/p[2]/label";
 
-    public static $import_msg_success   = "The import has successfully been completed.";
+	public static $import_msg_success   = "The import has successfully been completed.";
 
-    public static $export_csv_confirmed   = ".//*[@id='status1']";
-    public static $export_csv_unconfirmed = ".//*[@id='status0']";
-    public static $export_csv_testers     = ".//*[@id='status9']";
+	public static $export_csv_confirmed   = ".//*[@id='status1']";
+	public static $export_csv_unconfirmed = ".//*[@id='status0']";
+	public static $export_csv_testers     = ".//*[@id='status9']";
 
-    public static $export_csv_unarchived = ".//*[@id='archive0']";
-    public static $export_csv_archived   = ".//*[@id='archive1']";
+	public static $export_csv_unarchived = ".//*[@id='archive0']";
+	public static $export_csv_archived   = ".//*[@id='archive1']";
 
-    public static $export_legend_fields = ".//*[@id='adminForm']/fieldset/div/table/tbody/tr[5]/td[1]";
+	public static $export_legend_fields = ".//*[@id='adminForm']/fieldset/div/table/tbody/tr[5]/td[1]";
 
-    public static $export_button_up     = ".//*[@id='adminForm']/fieldset/div/table/tbody/tr[5]/td[2]/span[1]/input";
-    public static $export_button_down   = ".//*[@id='adminForm']/fieldset/div/table/tbody/tr[5]/td[2]/span[2]/input";
-    public static $export_button_remove = ".//*[@id='adminForm']/fieldset/div/table/tbody/tr[5]/td[2]/span[3]/input";
-    public static $export_button_export = ".//*[@id='adminForm']/fieldset/div/table/tbody/tr[6]/td/input";
+	public static $export_button_up     = ".//*[@id='adminForm']/fieldset/div/table/tbody/tr[5]/td[2]/span[1]/input";
+	public static $export_button_down   = ".//*[@id='adminForm']/fieldset/div/table/tbody/tr[5]/td[2]/span[2]/input";
+	public static $export_button_remove = ".//*[@id='adminForm']/fieldset/div/table/tbody/tr[5]/td[2]/span[3]/input";
+	public static $export_button_export = ".//*[@id='adminForm']/fieldset/div/table/tbody/tr[6]/td/input";
 
-    /**
+	/**
 	 * @param \AcceptanceTester $I
 	 * @param boolean           $activated
 	 *

2c9b1451adcf7a68deb6fa6fdf24a927787874a9
Romana Boldt <webmaster@boldt-webservice.de>
2018-02-09 08:57:21 +0100

    adjustments to meet coding standard

--- src/administrator/components/com_bwpostman/views/medialist/tmpl/default.php
+++ src/administrator/components/com_bwpostman/views/medialist/tmpl/default.php
@@ -30,20 +30,21 @@ defined('_JEXEC') or die;
 if (count($this->images) > 0 || count($this->folders) > 0 || count($this->documents) > 0)
 { ?>
 	<ul class="manager thumbnails">
-		<?php for ($i = 0, $n = count($this->folders); $i < $n; $i++) :
-				$this->setFolder($i);
-				echo $this->loadTemplate('folder');
-			endfor; ?>
+		<?php
+		for ($i = 0, $n = count($this->folders); $i < $n; $i++) :
+			$this->setFolder($i);
+			echo $this->loadTemplate('folder');
+		endfor; ?>
 
-			<?php for ($i = 0, $n = count($this->images); $i < $n; $i++) :
-				$this->setImage($i);
-				echo $this->loadTemplate('image');
-			endfor; ?>
+		<?php for ($i = 0, $n = count($this->images); $i < $n; $i++) :
+			$this->setImage($i);
+			echo $this->loadTemplate('image');
+		endfor; ?>
 
-			<?php for ($i = 0, $n = count($this->documents); $i < $n; $i++) :
-				$this->setDocument($i);
-				echo $this->loadTemplate('document');
-			endfor; ?>
+		<?php for ($i = 0, $n = count($this->documents); $i < $n; $i++) :
+			$this->setDocument($i);
+			echo $this->loadTemplate('document');
+		endfor; ?>
 	</ul>
 
 <?php

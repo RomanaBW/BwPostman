7713ce76502141313c761c6789dfe001bc09536f
Romana Boldt <webmaster@boldt-webservice.de>
2018-09-21 10:44:47 +0200

    $ replaced by jQuery

--- tests/acceptance/Backend/TestOptionsCest.php
+++ tests/acceptance/Backend/TestOptionsCest.php
@@ -114,7 +114,7 @@ codecept_debug("Value: $value");
 
 				$I->scrollTo($identifier, 0, -150);
 				$I->waitForElementVisible($identifier, 30);
-				$I->executeJS(' $(\''.$identifier.' option\').removeAttr("selected"); $(\'' . $identifier . ' option[value="' . $value . '"]\').attr("selected", true); $(\'' . $identifier . '\').change(); ');
+				$I->executeJS(' jQuery(\''.$identifier.' option\').removeAttr("selected"); $(\'' . $identifier . ' option[value="' . $value . '"]\').attr("selected", true); $(\'' . $identifier . '\').change(); ');
 				$I->selectOption($identifier, $value);
 				$I->wait(1);
 			}

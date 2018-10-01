45adca3c549066750a2841fda68493b046ae8c9e
Romana Boldt <webmaster@boldt-webservice.de>
2018-10-01 22:02:54 +0200

    workaround removed

---
+++ tests/unit/administrator/components/com_bwpostman/models/BwPostmanModelCampaignTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @package     administrator\components\com_bwpostman\models
+ * @subpackage
+ *
+ * @copyright   A copyright
+ * @license     A "Slug" license name e.g. GPL2
+ */
+
+namespace administrator\components\com_bwpostman\models;
+
+use BwPostmanModelCampaign;
+
+class BwPostmanModelCampaignTest extends \PHPUnit_Framework_TestCase
+{
+
+	public function testGetTable()
+	{
+
+	}
+
+	public function testGetNewsletters()
+	{
+
+	}
+
+	public function testSetId()
+	{
+
+	}
+
+	public function testDelete()
+	{
+
+	}
+
+	public function testGetForm()
+	{
+
+	}
+
+	public function test__construct()
+	{
+
+	}
+
+	public function testGetItem()
+	{
+
+	}
+
+	public function testArchive()
+	{
+
+	}
+
+	public function testSave()
+	{
+
+	}
+}

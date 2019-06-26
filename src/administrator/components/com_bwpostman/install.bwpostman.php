e5c24eabce7d02d3928973e40d5a2d7a77b57651
Romana Boldt <webmaster@boldt-webservice.de>
2019-06-26 15:45:06 +0200

    123 Make BwPostman Joomla 4 ready (legacy), installation files

--- src/administrator/components/com_bwpostman/install.bwpostman.php
+++ src/administrator/components/com_bwpostman/install.bwpostman.php
@@ -833,14 +833,14 @@ class Com_BwPostmanInstallerScript
 			if(version_compare($jversion->getShortVersion(), '3.99', 'ge'))
 			{
 				$groupModel = new Joomla\Component\Users\Administrator\Model\GroupModel();
+				$this->logger->addEntry(new JLogEntry('GroupModel 1: ' . var_dump($groupModel), JLog::DEBUG, $this->log_cat));
 			}
 			else
 			{
 				JModelLegacy::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_users/models');
 				$groupModel = JModelLegacy::getInstance('Group', 'UsersModel');
+				$this->logger->addEntry(new JLogEntry('GroupModel 2: ' . var_dump($groupModel), JLog::DEBUG, $this->log_cat));
 			}
-
-			$this->logger->addEntry(new JLogEntry('GroupModel 1: ' . var_dump($groupModel), JLog::DEBUG, $this->log_cat));
 
 			// get group ID of public
 			$public_id = $this->getGroupId('Public');
@@ -853,7 +853,7 @@ class Com_BwPostmanInstallerScript
 			{
 				if(version_compare($jversion->getShortVersion(), '3.99', 'ge'))
 				{
-					$this->logger->addEntry(new JLogEntry('GroupModel 2: ' . var_dump(Joomla\Component\Users\Administrator\Model\GroupModel), JLog::DEBUG, $this->log_cat));
+					$this->logger->addEntry(new JLogEntry('GroupModel 3: ' . var_dump(Joomla\Component\Users\Administrator\Model\GroupModel), JLog::DEBUG, $this->log_cat));
 
 					$ret = GroupModel::save(array('id' => 0, 'parent_id' => $public_id, 'title' => 'BwPostmanAdmin'));
 				}

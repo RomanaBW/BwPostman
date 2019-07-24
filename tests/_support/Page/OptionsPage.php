c5421ae37d3cc1d3923c82ec869fde1080019c61
Romana Boldt <webmaster@boldt-webservice.de>
2019-07-24 16:08:57 +0200

    123 Make BwPostman Joomla 4 ready (legacy), update options tests

--- tests/_support/Page/OptionsPage.php
+++ tests/_support/Page/OptionsPage.php
@@ -2243,10 +2243,11 @@ class OptionsPage
 
 		$I->clickAndWait(Generals::$toolbar4['Options'], 1);
 
-		$I->clickAndWait(Generals::$toolbar['Save & Close'], 1);
-
+		$I->click(Generals::$toolbar['Save']);
 		$I->waitForElementVisible(Generals::$alert_header, 15);
 		$I->see("Message", Generals::$alert_header);
 		$I->see(Generals::$alert_msg_txt, Generals::$alert_success);
+
+		$I->clickAndWait(Generals::$toolbar['Save & Close'], 1);
 	}
 }

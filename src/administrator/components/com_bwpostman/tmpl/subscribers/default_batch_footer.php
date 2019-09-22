eec0c64865bd6a984f245551266d3b1c9a537242
Romana Boldt <webmaster@boldt-webservice.de>
2019-09-22 16:08:15 +0200

    123 Make BwPostman Joomla 4 ready (legacy)

---
+++ src/administrator/components/com_bwpostman/tmpl/subscribers/default_batch_footer.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_content
+ *
+ * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+
+HTMLHelper::_('script', 'bw_postman/admin-subscribers-default-batch-footer.es6.js', ['version' => 'auto', 'relative' => true]);
+?>
+<button type="button" class="btn btn-secondary" data-dismiss="modal">
+	<?php echo Text::_('JCANCEL'); ?>
+</button>
+<button type="submit" id='batch-submit-button-id' class="btn btn-success" data-submit-task='subscriber.batch'>
+	<?php echo Text::_('JGLOBAL_BATCH_PROCESS'); ?>
+</button>

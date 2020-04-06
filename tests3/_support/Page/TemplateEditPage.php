5b780aabbe9e20f75205e145180c3a66652190c7
Romana Boldt <webmaster@boldt-webservice.de>
2020-04-06 22:23:25 +0200

    debugging messages removed

--- tests3/_support/Page/TemplateEditPage.php
+++ tests3/_support/Page/TemplateEditPage.php
@@ -571,8 +571,6 @@ class TemplateEditPage
 		}
 		catch (\Exception $e)
 		{
-			codecept_debug("Show exception:");
-			codecept_debug($e);
 			$I->switchToIFrame(Generals::$image_frame);
 
 			$I->waitForElementVisible(".//ul[contains(@class, 'manager')]", 5);

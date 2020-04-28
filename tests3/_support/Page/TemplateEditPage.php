ee66ae449a4e48676e18f317f8b23530d3993467
Romana Boldt <webmaster@boldt-webservice.de>
2020-04-28 16:58:49 +0200

    debug selection of thumbnail at template details tests

--- tests3/_support/Page/TemplateEditPage.php
+++ tests3/_support/Page/TemplateEditPage.php
@@ -535,24 +535,24 @@ class TemplateEditPage
 
 		$I->clickAndWait(self::$thumb_insert, 1);
 
-		try
-		{
-			$I->wait(1);
-		}
-		catch (\Exception $e)
-		{
-			codecept_debug("Show exception:");
-//			codecept_debug($e);
-			$I->switchToIFrame(Generals::$image_frame);
-
-			$I->waitForElementVisible(".//ul[contains(@class, 'manager')]", 5);
-			$I->clickAndWait(self::$thumb_select_user, 1);
-
-			$I->switchToIFrame();
-			$I->switchToIFrame(Generals::$media_frame);
-
-			$I->clickAndWait(self::$thumb_insert_user, 1);
-		}
+//		try
+//		{
+//			$I->wait(1);
+//		}
+//		catch (\Exception $e)
+//		{
+//			codecept_debug("Show exception:");
+////			codecept_debug($e);
+//			$I->switchToIFrame(Generals::$image_frame);
+//
+//			$I->waitForElementVisible(".//ul[contains(@class, 'manager')]", 5);
+//			$I->clickAndWait(self::$thumb_select_user, 1);
+//
+//			$I->switchToIFrame();
+//			$I->switchToIFrame(Generals::$media_frame);
+//
+//			$I->clickAndWait(self::$thumb_insert_user, 1);
+//		}
 
 		$I->switchToIFrame();
 	}

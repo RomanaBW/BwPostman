d8c525a60fa9392770e274b18d8412fbbea197b9
Romana Boldt <webmaster@boldt-webservice.de>
2020-10-06 16:09:54 +0200

    Correct namespace

--- src/administrator/components/com_bwpostman/libraries/BwEmailValidation.php
+++ src/administrator/components/com_bwpostman/libraries/BwEmailValidation.php
@@ -24,7 +24,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace BoldtWebservice\Component\BwPostman\Administrator\Helper;
+namespace BoldtWebservice\Component\BwPostman\Administrator\Libraries;
 
 defined('_JEXEC') or die('Restricted access');
 

15708f13166d94bfb1a2a710f66c1a3202d99b0e
Romana Boldt <webmaster@boldt-webservice.de>
2021-07-10 16:02:00 +0200

    Remove namespace from installer script

--- src/administrator/components/com_bwpostman/com_bwpostmanInstallerScript.php
+++ src/administrator/components/com_bwpostman/com_bwpostmanInstallerScript.php
@@ -24,13 +24,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-namespace BoldtWebservice\Component\BwPostman\Administrator;
+//namespace BoldtWebservice\Component\BwPostman\Administrator;
 
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
 
-use Exception;
-use JLoader;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Session\Session;
@@ -50,7 +48,6 @@ use Joomla\Component\Users\Administrator\Model\LevelModel;
 use BoldtWebservice\Component\BwPostman\Administrator\Helper\BwPostmanInstallHelper;
 use BoldtWebservice\Component\BwPostman\Administrator\Libraries\BwLogger;
 use BoldtWebservice\Component\BwPostman\Administrator\Libraries\BwException;
-use RuntimeException;
 
 /**
  * Class Com_BwPostmanInstallerScript

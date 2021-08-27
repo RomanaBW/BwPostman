1222caf98bf5d85af0a6068251703acaa7323c39
Romana Boldt <webmaster@boldt-webservice.de>
2021-08-27 14:24:21 +0200

    Change class header to BwPostman

--- src/administrator/components/com_bwpostman/layouts/toolbar/extlink.php
+++ src/administrator/components/com_bwpostman/layouts/toolbar/extlink.php
@@ -1,10 +1,27 @@
 <?php
 /**
- * @package     Joomla.Site
- * @subpackage  Layout
+ * BwPostman Newsletter Component
  *
- * @copyright   Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * BwPostman external link layout
+ *
+ * @version %%version_number%%
+ * @package BwPostman-Admin
+ * @author Romana Boldt
+ * @copyright (C) %%copyright_year%% Boldt Webservice <forum@boldt-webservice.de>
+ * @support https://www.boldt-webservice.de/en/forum-en/forum/bwpostman.html
+ * @license GNU/GPL, see LICENSE.txt
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 defined('JPATH_BASE') or die;

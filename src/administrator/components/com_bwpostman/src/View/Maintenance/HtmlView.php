1e1c3d098200f1109e22ac57a1e691f1a74be12f
Romana Boldt <webmaster@boldt-webservice.de>
2021-09-06 10:01:16 +0200

    Undo removing of get document

--- src/administrator/components/com_bwpostman/src/View/Maintenance/HtmlView.php
+++ src/administrator/components/com_bwpostman/src/View/Maintenance/HtmlView.php
@@ -248,6 +248,7 @@ class HtmlView extends BaseHtmlView
 
 		// Get the toolbar object instance
 		$toolbar = Toolbar::getInstance();
+		$document = $app->getDocument();
 
 		// Set toolbar title
 		ToolbarHelper::title(Text::_('COM_BWPOSTMAN_MAINTENANCE'), 'wrench');

222f33d16cb7aab1d18133f70ba97233527d9ea0
Romana Boldt <webmaster@boldt-webservice.de>
2021-11-18 06:45:53 +0100

    Remove default setting from text columns at update

--- src/administrator/components/com_bwpostman/sql/updates/mysql/4.0.1.sql
+++ src/administrator/components/com_bwpostman/sql/updates/mysql/4.0.1.sql
@@ -1,1 +1,33 @@
 /* Placeholder file for database changes for version 4.0.1 to satisfy Joomla!'s extensions installer */
+
+ALTER TABLE `#__bwpostman_newsletters` ALTER `intro_text` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_newsletters` ALTER `intro_text_text` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_newsletters` ALTER `html_version` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_newsletters` ALTER `text_version` DROP DEFAULT;
+
+ALTER TABLE `#__bwpostman_sendmailcontent` ALTER `body` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_sendmailcontent` ALTER `attachment` DROP DEFAULT;
+
+ALTER TABLE `#__bwpostman_templates` ALTER `tpl_html` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates` ALTER `tpl_css` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates` ALTER `tpl_article` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates` ALTER `tpl_divider` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates` ALTER `intro` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates` ALTER `article` DROP DEFAULT;
+
+ALTER TABLE `#__bwpostman_templates_tpl` ALTER `css` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tpl` ALTER `header_tpl` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tpl` ALTER `intro_tpl` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tpl` ALTER `divider_tpl` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tpl` ALTER `article_tpl` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tpl` ALTER `readon_tpl` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tpl` ALTER `footer_tpl` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tpl` ALTER `button_tpl` DROP DEFAULT;
+
+ALTER TABLE `#__bwpostman_templates_tags` ALTER `tpl_tags_head_advanced` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tags` ALTER `tpl_tags_body_advanced` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tags` ALTER `tpl_tags_article_advanced_b` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tags` ALTER `tpl_tags_article_advanced_e` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tags` ALTER `tpl_tags_readon_advanced` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tags` ALTER `tpl_tags_legal_advanced_b` DROP DEFAULT;
+ALTER TABLE `#__bwpostman_templates_tags` ALTER `tpl_tags_legal_advanced_e` DROP DEFAULT;

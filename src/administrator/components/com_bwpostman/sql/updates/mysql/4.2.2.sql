0378cf83666bb9a54ee637a4254a6ec672ec8370
Romana Boldt <webmaster@boldt-webservice.de>
2023-07-17 08:12:58 +0200

    Remove NOT NULL from DATETIME columns which may hold NULL

--- src/administrator/components/com_bwpostman/sql/updates/mysql/4.2.2.sql
+++ src/administrator/components/com_bwpostman/sql/updates/mysql/4.2.2.sql
@@ -1,1 +1,25 @@
-/* Placeholder file for database changes for version 4.2.2 to satisfy Joomla!'s extensions installer */
+/* Remove NOT NULL from DATETIME columns which may hold NULL */
+
+ALTER TABLE `#__bwpostman_campaigns` MODIFY `modified_time` DATETIME;
+ALTER TABLE `#__bwpostman_campaigns` MODIFY `checked_out_time` DATETIME;
+ALTER TABLE `#__bwpostman_campaigns` MODIFY `archive_date` DATETIME;
+
+ALTER TABLE `#__bwpostman_mailinglists` MODIFY `modified_time` DATETIME;
+ALTER TABLE `#__bwpostman_mailinglists` MODIFY `checked_out_time` DATETIME;
+ALTER TABLE `#__bwpostman_mailinglists` MODIFY `archive_date` DATETIME;
+
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `modified_time` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `mailing_date` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `publish_up` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `publish_down` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `checked_out_time` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `archive_date` DATETIME;
+
+ALTER TABLE `#__bwpostman_subscribers` MODIFY `confirmation_date` DATETIME;
+ALTER TABLE `#__bwpostman_subscribers` MODIFY `modified_time` DATETIME;
+ALTER TABLE `#__bwpostman_subscribers` MODIFY `checked_out_time` DATETIME;
+ALTER TABLE `#__bwpostman_subscribers` MODIFY `archive_date` DATETIME;
+
+ALTER TABLE `#__bwpostman_templates` MODIFY `modified_time` DATETIME;
+ALTER TABLE `#__bwpostman_templates` MODIFY `checked_out_time` DATETIME;
+ALTER TABLE `#__bwpostman_templates` MODIFY `archive_date` DATETIME;

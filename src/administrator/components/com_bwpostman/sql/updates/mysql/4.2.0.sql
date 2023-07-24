a7e5c2be5fa8f9778eb08f106d6a198bc0db81e7
Romana Boldt <webmaster@boldt-webservice.de>
2023-07-24 19:37:29 +0200

    Changes to meet Joomla!s needs

--- src/administrator/components/com_bwpostman/sql/updates/mysql/4.2.0.sql
+++ src/administrator/components/com_bwpostman/sql/updates/mysql/4.2.0.sql
@@ -4,31 +4,31 @@ ALTER TABLE `#__bwpostman_templates` MODIFY `basics` TEXT NOT NULL;
 
 /* Remove default value from DATETIME columns */
 
-ALTER TABLE `#__bwpostman_campaigns` MODIFY `created_date` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_campaigns` MODIFY `modified_time` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_campaigns` MODIFY `checked_out_time` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_campaigns` MODIFY `archive_date` DATETIME NOT NULL;
+ALTER TABLE `#__bwpostman_campaigns` MODIFY `created_date` DATETIME;
+ALTER TABLE `#__bwpostman_campaigns` MODIFY `modified_time` DATETIME;
+ALTER TABLE `#__bwpostman_campaigns` MODIFY `checked_out_time` DATETIME;
+ALTER TABLE `#__bwpostman_campaigns` MODIFY `archive_date` DATETIME;
 
-ALTER TABLE `#__bwpostman_mailinglists` MODIFY `created_date` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_mailinglists` MODIFY `modified_time` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_mailinglists` MODIFY `checked_out_time` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_mailinglists` MODIFY `archive_date` DATETIME NOT NULL;
+ALTER TABLE `#__bwpostman_mailinglists` MODIFY `created_date` DATETIME;
+ALTER TABLE `#__bwpostman_mailinglists` MODIFY `modified_time` DATETIME;
+ALTER TABLE `#__bwpostman_mailinglists` MODIFY `checked_out_time` DATETIME;
+ALTER TABLE `#__bwpostman_mailinglists` MODIFY `archive_date` DATETIME;
 
-ALTER TABLE `#__bwpostman_newsletters` MODIFY `created_date` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_newsletters` MODIFY `modified_time` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_newsletters` MODIFY `mailing_date` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_newsletters` MODIFY `publish_up` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_newsletters` MODIFY `publish_down` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_newsletters` MODIFY `checked_out_time` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_newsletters` MODIFY `archive_date` DATETIME NOT NULL;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `created_date` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `modified_time` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `mailing_date` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `publish_up` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `publish_down` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `checked_out_time` DATETIME;
+ALTER TABLE `#__bwpostman_newsletters` MODIFY `archive_date` DATETIME;
 
-ALTER TABLE `#__bwpostman_subscribers` MODIFY `registration_date` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_subscribers` MODIFY `confirmation_date` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_subscribers` MODIFY `modified_time` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_subscribers` MODIFY `checked_out_time` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_subscribers` MODIFY `archive_date` DATETIME NOT NULL;
+ALTER TABLE `#__bwpostman_subscribers` MODIFY `registration_date` DATETIME;
+ALTER TABLE `#__bwpostman_subscribers` MODIFY `confirmation_date` DATETIME;
+ALTER TABLE `#__bwpostman_subscribers` MODIFY `modified_time` DATETIME;
+ALTER TABLE `#__bwpostman_subscribers` MODIFY `checked_out_time` DATETIME;
+ALTER TABLE `#__bwpostman_subscribers` MODIFY `archive_date` DATETIME;
 
-ALTER TABLE `#__bwpostman_templates` MODIFY `created_date` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_templates` MODIFY `modified_time` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_templates` MODIFY `checked_out_time` DATETIME NOT NULL;
-ALTER TABLE `#__bwpostman_templates` MODIFY `archive_date` DATETIME NOT NULL;
+ALTER TABLE `#__bwpostman_templates` MODIFY `created_date` DATETIME;
+ALTER TABLE `#__bwpostman_templates` MODIFY `modified_time` DATETIME;
+ALTER TABLE `#__bwpostman_templates` MODIFY `checked_out_time` DATETIME;
+ALTER TABLE `#__bwpostman_templates` MODIFY `archive_date` DATETIME;

d22fa8de9235caad249fe927eef5fe926ebe8b95
Romana Boldt <webmaster@boldt-webservice.de>
2023-08-21 05:38:53 +0200

    Enable NULL for created_date/registration_date

--- src/administrator/components/com_bwpostman/sql/updates/mysql/4.2.4.sql
+++ src/administrator/components/com_bwpostman/sql/updates/mysql/4.2.4.sql
@@ -1,1 +1,16 @@
-/* Placeholder file for database changes for version 4.2.4 to satisfy Joomla!'s extensions installer */
+/* Enable NULL for created_date/registration_date */
+
+ALTER TABLE `#__bwpostman_campaigns`
+	CHANGE `created_date` `created_date` DATETIME NULL;
+
+ALTER TABLE `#__bwpostman_mailinglists`
+	CHANGE `created_date` `created_date` DATETIME NULL;
+
+ALTER TABLE `#__bwpostman_newsletters`
+	CHANGE `created_date` `created_date` DATETIME NULL;
+
+ALTER TABLE `#__bwpostman_subscribers`
+	CHANGE `registration_date` `registration_date` DATETIME NULL;
+
+ALTER TABLE `#__bwpostman_templates`
+	CHANGE `created_date` `created_date` DATETIME NULL;

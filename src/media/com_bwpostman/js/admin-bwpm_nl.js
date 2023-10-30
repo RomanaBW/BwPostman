276f5f0f2e023c179ef00fd01552c614b2ee2bf0
Romana Boldt <webmaster@boldt-webservice.de>
2023-10-30 06:17:57 +0100

    Correct syntax

--- src/media/com_bwpostman/js/admin-bwpm_nl.js
+++ src/media/com_bwpostman/js/admin-bwpm_nl.js
@@ -288,7 +288,7 @@ function moveArticle() {
 		}
 
 		// Empty input field
-		document.querySelector('.js-modal-content-select-field .btn-secondary').click()
+		document.querySelector('.js-modal-content-select-field .btn-secondary').click();
 		window.processModalParent('jform_ac_id');
 	}
 	else

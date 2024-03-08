d9cb9379a70ab4e8a1c60dcdb2b67eed8fabec9e
Romana Boldt <webmaster@boldt-webservice.de>
2024-03-08 12:34:12 +0100

    Create containers for J5.0.3

---
+++ build/docker/dev/J503/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2024-03-08 10:46:50 UTC
+#Software: Joomla! 5.0.3 Stable [ Kuboresha ] 20-February-2024 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2024-03-08T10:46:50+00:00	INFO 172.18.5.17	joomlafailure	Username and password do not match or you do not have an account yet.
+2024-03-08T10:47:04+00:00	INFO 172.18.5.17	joomlafailure	Username and password do not match or you do not have an account yet.

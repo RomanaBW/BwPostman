c23a3a5bc77655534224cd686b4bc1adda739a2a
Romana Boldt <webmaster@boldt-webservice.de>
2024-04-19 13:41:24 +0200

    Switch tester to J5.0.3

--- build/docker/test/configuration.php
+++ build/docker/test/configuration.php
@@ -4,7 +4,7 @@ class JConfig {
 	public $offline_message = 'Diese Website ist zurzeit im Wartungsmodus.<br />Bitte später wiederkommen.';
 	public $display_offline_message = 1;
 	public $offline_image = '';
-	public $sitename = 'Test502_BwPM_Container';
+	public $sitename = 'Test503_BwPM_Container';
 	public $editor = 'tinymce';
 	public $captcha = '0';
 	public $list_limit = 20;
@@ -13,7 +13,7 @@ class JConfig {
 	public $debug_lang = false;
 	public $debug_lang_const = true;
 	public $dbtype = 'mysqli';
-	public $host = '172.118.5.10';
+	public $host = '172.118.5.18';
 	public $user = 'tester';
 	public $password = 'barbamama';
 	public $db = 'joomlatest';
@@ -34,7 +34,7 @@ class JConfig {
 	public $mailonline = true;
 	public $mailer = 'smtp';
 	public $mailfrom = 'webmaster@boldt-webservice.de';
-	public $fromname = 'Test502_BwPM_Container';
+	public $fromname = 'Test503_BwPM_Container';
 	public $sendmail = '/usr/sbin/sendmail';
 	public $smtpauth = false;
 	public $smtpuser = '';

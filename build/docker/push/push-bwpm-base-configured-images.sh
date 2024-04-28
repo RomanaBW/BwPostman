a71d16608fe371f94a495aed083281e1165ad383
Romana Boldt <webmaster@boldt-webservice.de>
2024-04-28 11:34:25 +0200

    Introduce images preconfigured especially for BwPostman

---
+++ build/docker/push/push-bwpm-base-configured-images.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# This is part of step ?? to nail down the created images with installed and adjusted Joomla, preconfigured for BwPostman
+#
+# Tag is php version and Joomla version (with dots), separated by a hyphen
+#
+
+PHP_VERSION=8.1.0
+JOOMLA_VERSION=5.1.0
+
+docker commit -a 'Romana Boldt info@boldt-webservice.de' push-web universe3:5000/romana/bwpm-configured-files:$PHP_VERSION-$JOOMLA_VERSION
+docker commit -a 'Romana Boldt info@boldt-webservice.de' push-db universe3:5000/romana/bwpm-configured-tables:$PHP_VERSION-$JOOMLA_VERSION
+
+docker push universe3:5000/romana/bwpm-configured-files:$PHP_VERSION-$JOOMLA_VERSION
+docker push universe3:5000/romana/bwpm-configured-tables:$PHP_VERSION-$JOOMLA_VERSION

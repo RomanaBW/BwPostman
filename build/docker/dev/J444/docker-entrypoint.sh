1961ac5779ae90b382561e658d428a57f017b65f
Romana Boldt <webmaster@boldt-webservice.de>
2024-05-03 08:36:23 +0200

    Add creation of middleware for J444

---
+++ build/docker/dev/J444/docker-entrypoint.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -e
+
+if [ -n "$JOOMLA_DB_PASSWORD_FILE" ] && [ -f "$JOOMLA_DB_PASSWORD_FILE" ]; then
+        JOOMLA_DB_PASSWORD=$(cat "$JOOMLA_DB_PASSWORD_FILE")
+fi
+
+if [ -n "$JOOMLA_ADMIN_PASSWORD_FILE" ] && [ -f "$JOOMLA_ADMIN_PASSWORD_FILE" ]; then
+        JOOMLA_ADMIN_PASSWORD=$(cat "$JOOMLA_ADMIN_PASSWORD_FILE")
+fi
+
+WORK_DIR=$(pwd)
+
+echo >&2
+echo >&2 "========================================================================"
+echo >&2
+
+if [ -f /var/www/html/administrator/cache/autoload_psr4.php ];
+  then
+  # Clear class cache of Joomla
+  echo >&2 "Clear class cache of Joomla…"
+  echo >&2
+
+  rm /var/www/html/administrator/cache/autoload_psr4.php
+
+  echo >&2 "Class cache of Joomla cleared."
+  echo >&2
+fi
+
+# Add BwPostman user groups, map users to them
+echo >&2 "Add BwPostman user groups, map users to them…"
+echo >&2
+
+echo >&2 "DB-Params: $JOOMLA_DB_HOST $JOOMLA_DB_USER $JOOMLA_DB_PASSWORD $JOOMLA_DB_NAME $JOOMLA_DB_TYPE"
+echo >&2
+
+php /usr/src/files/adjustdb_bwpm.php "$JOOMLA_DB_HOST" "$JOOMLA_DB_USER" "$JOOMLA_DB_PASSWORD" "$JOOMLA_DB_NAME" "$JOOMLA_DB_TYPE"
+
+echo >&2 "…BwPostman user groups added, users added, mapping users to groups adjusted."
+
+# Ensure files of Joomla are writable
+echo >&2 "Ensure files of Joomla are writable…"
+
+chown -R www-data:www-data ./
+
+echo >&2 "Files are writable."
+echo >&2
+
+
+echo >&2
+echo >&2 "The Joomla installation and BwPostman should now work as expected."
+echo >&2
+echo >&2 "========================================================================"
+echo >&2
+
+exec "$@"

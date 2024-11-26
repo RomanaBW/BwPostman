0bb53ac6cdca0daf98961ed3918534e6f3078a4d
Romana Boldt <webmaster@boldt-webservice.de>
2024-11-27 00:09:47 +0100

    Update to J 5.2.2

--- build/docker/push-current/push-bwpm-configured-images.sh
+++ build/docker/push-current/push-bwpm-configured-images.sh
@@ -5,14 +5,14 @@
 # Tag is php version and Joomla version (with dots), separated by a hyphen
 #
 
-docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-current-joomla universe3:5000/romana/bwpm-configured-files:8.1.0-5.2.1
-docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-current-db universe3:5000/romana/bwpm-configured-tables:8.1.0-5.2.1
+docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-current-joomla universe3:5000/romana/bwpm-configured-files:8.1.0-5.2.2
+docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-current-db universe3:5000/romana/bwpm-configured-tables:8.1.0-5.2.2
 
 docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-current-joomla universe3:5000/romana/bwpm-configured-files-8.1.0:latest
 docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-current-db universe3:5000/romana/bwpm-configured-tables-8.1.0:latest
 
-docker push universe3:5000/romana/bwpm-configured-files:8.1.0-5.2.1
-docker push universe3:5000/romana/bwpm-configured-tables:8.1.0-5.2.1
+docker push universe3:5000/romana/bwpm-configured-files:8.1.0-5.2.2
+docker push universe3:5000/romana/bwpm-configured-tables:8.1.0-5.2.2
 
 docker push universe3:5000/romana/bwpm-configured-files-8.1.0:latest
 docker push universe3:5000/romana/bwpm-configured-tables-8.1.0:latest

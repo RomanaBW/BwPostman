10c3d2ac5f0a2b378fa52b9e948d3aae3853ca23
Romana Boldt <webmaster@boldt-webservice.de>
2025-02-01 11:06:53 +0100

    Switch to J5.2.3/4.4.10

--- build/docker/config-current/commit-bwpm-base-configured-images.sh
+++ build/docker/config-current/commit-bwpm-base-configured-images.sh
@@ -5,5 +5,5 @@
 # Tag is php version and Joomla version (with dots), separated by a hyphen
 #
 
-docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-config-current-joomla universe3:5000/romana/bwpm-base-files:8.1.0-5.2.2
-docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-config-current-db universe3:5000/romana/bwpm-base-tables:8.1.0-5.2.2
+docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-config-current-joomla universe3:5000/romana/bwpm-base-files:8.1.0-5.2.3
+docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-config-current-db universe3:5000/romana/bwpm-base-tables:8.1.0-5.2.3
--- build/docker/push-previous/push-bwpm-configured-images.sh
+++ build/docker/push-previous/push-bwpm-configured-images.sh
@@ -5,14 +5,14 @@
 # Tag is php version and Joomla version (with dots), separated by a hyphen
 #
 
-docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-previous-joomla universe3:5000/romana/bwpm-configured-files:7.4.0-4.4.9
-docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-previous-db universe3:5000/romana/bwpm-configured-tables:7.4.0-4.4.9
+docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-previous-joomla universe3:5000/romana/bwpm-configured-files:7.4.0-4.4.10
+docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-previous-db universe3:5000/romana/bwpm-configured-tables:7.4.0-4.4.10
 
 docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-previous-joomla universe3:5000/romana/bwpm-configured-files-7.4.0:latest
 docker commit -a 'Romana Boldt info@boldt-webservice.de' bwpm-push-previous-db universe3:5000/romana/bwpm-configured-tables-7.4.0:latest
 
-docker push universe3:5000/romana/bwpm-configured-files:7.4.0-4.4.9
-docker push universe3:5000/romana/bwpm-configured-tables:7.4.0-4.4.9
+docker push universe3:5000/romana/bwpm-configured-files:7.4.0-4.4.10
+docker push universe3:5000/romana/bwpm-configured-tables:7.4.0-4.4.10
 
 docker push universe3:5000/romana/bwpm-configured-files-7.4.0:latest
 docker push universe3:5000/romana/bwpm-configured-tables-7.4.0:latest

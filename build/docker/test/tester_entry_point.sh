98982aa9f06da417312f5b16d75689891df8a37d
Romana Boldt <webmaster@boldt-webservice.de>
2025-09-02 11:13:19 +0200

    Switch to J5.3.3

--- build/docker/test/tester_entry_point.sh
+++ build/docker/test/tester_entry_point.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 whoami
 
-echo "Test-Cat: lists_nl"
-echo "Project: j531_bwpm433"
+echo "Test-Cat: withdraw_testdata"
+echo "Project: j533_bwpm433"
 
 sudo chmod 1777 /tmp/.X11-unix
 sudo rm -f /tmp/.X45-lock
@@ -10,7 +10,7 @@ sudo mkdir -p /home/seluser/.local/share/mc
 sudo chown -R seluser:users /home/seluser
 
 export BW_TEST_WITHDRAW="no"
-VIDEO_NAME=/data/output/videos/lists_nl.mp4
+VIDEO_NAME=/data/output/videos/withdraw_testdata.mp4
 VIDEO_LOG=/data/output/logs/ffmpeg.log
 
 DISPLAY=":45"
@@ -39,7 +39,7 @@ tmux new-session -d -s BwPostmanRecording1 "ffmpeg -y -f x11grab -draw_mouse 0 -
 
 # run tests
 cd /data
-tests/job_scripts/bwpm_test_runner.sh
+tests/job_scripts/bwpm_test_runner_withdraw.sh
 
 # stop video recording
 echo 'stop recording'

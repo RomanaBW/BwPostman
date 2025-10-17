be1e9c8b8cc0dbe5414824ab0798935d929c5e34
Romana Boldt <webmaster@boldt-webservice.de>
2025-10-17 12:41:28 +0200

    Remove double use entry

--- src/modules/mod_bwpostman/tmpl/default.php
+++ src/modules/mod_bwpostman/tmpl/default.php
@@ -34,7 +34,6 @@ use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\Component\Content\Site\Helper\RouteHelper;
 use BoldtWebservice\Component\BwPostman\Administrator\Helper\BwPostmanSubscriberHelper;
-use Joomla\Component\Content\Site\Helper\RouteHelper;
 
 HtmlHelper::_('behavior.keepalive');
 
